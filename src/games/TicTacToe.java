49b02a4bf8d3c01cfec8957345c1d502f165ab06
Ansh Agrawal <hsna.agrawal@gmail.com>
2024-05-06 19:18:28 -0400

    Win Condition for Tic Tac Toe

--- src/games/TicTacToe.java
+++ src/games/TicTacToe.java
@@ -11,6 +11,9 @@ public class TicTacToe extends JPanel {
     private static final int HEIGHT = 640;
     private static final int[][] BOARD = new int[3][3]; // 0 = None, 1 = X, 2 = O
     private static int CURRENT_PLAYER = 1;
+    private int winningLineType = 0; // 1 = horizontal, 2 = vertical, 3 = diagonal (top left to bottom right), 4 = diagonal (top right to bottom left)
+    private int winningLineIndex = 0;
+
 
     public TicTacToe() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
@@ -50,10 +53,31 @@ public class TicTacToe extends JPanel {
                 }
             }
         }
+
+        // Check win condition and draw winning line
+        if (checkWinCondition()) {
+            g2d.setColor(Color.BLACK);
+            if (winningLineType == 1) {
+                // Draw horizontal line
+                g.drawLine(90, 300 + (winningLineIndex * 100), 390, 300 + (winningLineIndex * 100));
+            } else if (winningLineType == 2) {
+                // Draw vertical line
+                g.drawLine(140 + (winningLineIndex * 100), 250, 140 + (winningLineIndex * 100), 550);
+            } else if (winningLineType == 3) {
+                // Draw diagonal line from top left to bottom right
+                g.drawLine(100, 260, 380, 540);
+            } else if (winningLineType == 4) {
+                // Draw diagonal line from top right to bottom left
+                g.drawLine(380, 260, 100, 540);
+            }
+        }
     }
 
+
     class TicTacToeMouse implements MouseListener {
-        public void mouseClicked(MouseEvent e) {
+        public void mouseClicked(MouseEvent e) {}
+
+        public void mousePressed(MouseEvent e) {
             int x = e.getX();
             int y = e.getY();
 
@@ -94,8 +118,6 @@ public class TicTacToe extends JPanel {
             }
         }
 
-        public void mousePressed(MouseEvent e) {}
-
         public void mouseReleased(MouseEvent e) {}
 
         public void mouseEntered(MouseEvent e) {}
@@ -113,5 +135,41 @@ public class TicTacToe extends JPanel {
             repaint();
             CURRENT_PLAYER = 1;
         }
+        if (checkWinCondition()) {
+            System.out.println("Win");
+        }
     }
+
+    private boolean checkWinCondition() {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (BOARD[i][0] == BOARD[i][1] && BOARD[i][1] == BOARD[i][2] && BOARD[i][0] != 0) {
+                winningLineType = 1; // Horizontal line
+                winningLineIndex = i;
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < 3; j++) {
+            if (BOARD[0][j] == BOARD[1][j] && BOARD[1][j] == BOARD[2][j] && BOARD[0][j] != 0) {
+                winningLineType = 2; // Vertical line
+                winningLineIndex = j;
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if ((BOARD[0][0] == BOARD[1][1] && BOARD[1][1] == BOARD[2][2] && BOARD[0][0] != 0)) {
+            winningLineType = 3; // Diagonal (top left to bottom right)
+            return true;
+        }
+        if (BOARD[0][2] == BOARD[1][1] && BOARD[1][1] == BOARD[2][0] && BOARD[0][2] != 0) {
+            winningLineType = 4; // Diagonal (top right to bottom left)
+            return true;
+        }
+
+        return false;
+    }
+
 }
