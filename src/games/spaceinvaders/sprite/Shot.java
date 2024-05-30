700984bf789fe76df65309cd0ed3e3c234c5df27
Ansh Agrawal <hsna.agrawal@gmail.com>
2024-05-30 10:45:43 -0400

    Added space invaders game

---
+++ src/games/spaceinvaders/sprite/Shot.java
@@ -0,0 +1,27 @@
+package games.spaceinvaders.sprite;
+
+import javax.swing.ImageIcon;
+
+public class Shot extends Sprite {
+
+    public Shot() {
+    }
+
+    public Shot(int x, int y) {
+
+        initShot(x, y);
+    }
+
+    private void initShot(int x, int y) {
+
+        var shotImg = "src/games/spaceinvaders/images/shot.png";
+        var ii = new ImageIcon(shotImg);
+        setImage(ii.getImage());
+
+        int H_SPACE = 6;
+        setX(x + H_SPACE);
+
+        int V_SPACE = 1;
+        setY(y - V_SPACE);
+    }
+}
